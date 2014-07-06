020e7f85a7effb2379979ab5267d422ef50ecf1e
Eric Sowell <eric.sowell@tx1-esowel09ml.home>
2014-07-06 18:14:18 -0500

    initial commit

---
+++ FlashcardWalkthrough/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  FlashcardWalkthrough
+//
+//  Created by Eric Sowell on 7/5/14.
+//  Copyright (c) 2014 EricSowell.com. All rights reserved.
+//
+
+class Card {
+    
+    let front: String
+    let back: String
+    
+    init(front: String, back: String) {
+        self.front = front
+        self.back = back
+    }
+}
--- FlashcardWalkthrough/ViewController.swift
+++ FlashcardWalkthrough/ViewController.swift
@@ -9,17 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet var cardLabel: UILabel
+    
+    let cards: Array<Card> = [
+        Card(front: "τόξον, τό", back: "bow"),
+        Card(front: "μνηστήρ, ὁ", back: "suitor"),
+        Card(front: "κλίμαξ, ἡ", back: "ladder")
+    ]
+    
+    var currentCard = 0
+    var showFront = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        showCard()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func nextTapped(sender: AnyObject) {
+        self.currentCard++
+        self.showFront = true
+        
+        if (self.currentCard >= self.cards.count) {
+            self.currentCard = self.cards.count - 1
+        }
+        
+        showCard()
     }
-
-
+    
+    @IBAction func backTapped(sender: AnyObject) {
+        self.currentCard--
+        self.showFront = true
+        
+        if (self.currentCard < 0) {
+            self.currentCard = 0
+        }
+        
+        showCard()
+    }
+    
+    @IBAction func flipCardTapped(sender: AnyObject) {
+        if (self.showFront) {
+            self.showFront = false
+        }
+        else {
+            self.showFront = true
+        }
+        showCard()
+    }
+    
+    func showCard() {
+        if (self.showFront) {
+            self.cardLabel.text = self.cards[self.currentCard].front
+        }
+        else {
+            self.cardLabel.text = self.cards[self.currentCard].back
+        }
+    }
 }
 
